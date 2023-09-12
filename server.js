7f286cb8c698e4f38fea97484c57f4413e350e5e
Viktor Malyshev <vmalyshev0@gmail.com>
2023-09-12 09:58:49 +0200

    added caching to dev

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
 const compression = require('compression')
+const { createRequestHandler } = require('@remix-run/express')
+
+const BUILD_DIR = path.join(process.cwd(), 'build')
 
 const app = express()
 
@@ -21,16 +25,55 @@ app.use(
         }
       }
       return !(contentType.includes('text/html') ||
-        contentType.includes('text/event-stream'));
+        contentType.includes('text/event-stream'))
     },
   })
 )
 
+// Remix fingerprints its assets so we can cache forever.
+app.use(
+  '/build',
+  express.static('public/build', { immutable: true, maxAge: '1y' })
+)
+
+// Everything else (like favicon.ico) is cached for an hour. You may want to be
+// more aggressive with this caching.
+app.use(express.static('public', { maxAge: '1h' }))
+
 app.use(morgan('tiny'))
 
+app.all(
+  '*',
+  process.env.NODE_ENV === 'development'
+    ? (req, res, next) => {
+      purgeRequireCache()
+
+      return createRequestHandler({
+        build: require(BUILD_DIR),
+        mode: process.env.NODE_ENV,
+      })(req, res, next)
+    }
+    : createRequestHandler({
+      build: require(BUILD_DIR),
+      mode: process.env.NODE_ENV,
+    })
+)
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
+
+function purgeRequireCache() {
+  // purge require cache on requests for "server side HMR" this won't let
+  // you have in-memory objects between requests in development,
+  // alternatively you can set up nodemon/pm2-dev to restart the server on
+  // file changes, but then you'll have to reconnect to databases/etc on each
+  // change. DX is better and this approach is clever, so it's included by default
+  for (const key in require.cache) {
+    if (key.startsWith(BUILD_DIR)) {
+      delete require.cache[key]
+    }
+  }
+}
