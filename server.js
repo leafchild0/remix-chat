fbf46a03f08b54afe8c8d741d5b0e60475f60485
Viktor Malyshev <vmalyshev0@gmail.com>
2023-09-12 09:56:16 +0200

    added compression to the server, basic one

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const morgan = require('morgan')
+const compression = require('compression')
 
 const app = express()
 
 // This is needs to be excluded, best practice
 //app.disable('x-powered-by')
+
+// Added compression
+app.use(
+  compression({
+    filter: (_req, res) => {
+      const contentTypeHeader = res.getHeader('Content-Type')
+      let contentType = ''
+      if (contentTypeHeader) {
+        if (Array.isArray(contentTypeHeader)) {
+          contentType = contentTypeHeader.join(' ')
+        } else {
+          contentType = String(contentTypeHeader)
+        }
+      }
+      return !(contentType.includes('text/html') ||
+        contentType.includes('text/event-stream'));
+    },
+  })
+)
 
 app.use(morgan('tiny'))
 
